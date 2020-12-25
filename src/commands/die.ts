603e3121f8b2dfc5d97d3eca073b126a0008469f
Kacper Stolarek <stolarek83@gmail.com>
2020-12-25 20:08:38 +0100

    Disabled channel guard in die :hankey:

--- src/commands/die.ts
+++ src/commands/die.ts
@@ -3,15 +3,16 @@ import { RedisLogic } from '../redisLogic';
 import { channelGuard } from '../utils/channelGuard';
 
 export const dieCommand = async (interaction: Discord.Interaction, db: RedisLogic, connections: Map<string, Discord.VoiceConnection>) => {
-    if (!channelGuard(interaction, connections)) {
-        const embed: Discord.MessageEmbed = new Discord.MessageEmbed()
-        .setColor(0x000000)
-        .setDescription(`
-        <@${interaction.author?.id}> you must be in a voice channel with me in order to kill me!
-        `);
-        interaction.channel.send(embed);
-        return;
-    }
+    // if (!channelGuard(interaction, connections)) {
+    //     const embed: Discord.MessageEmbed = new Discord.MessageEmbed()
+    //     .setColor(0x000000)
+    //     .setDescription(`
+    //     <@${interaction.author?.id}> you must be in a voice channel with me in order to kill me!
+    //     `);
+    //     interaction.channel.send(embed);
+    //     return;
+    // }  Disabled due to some issues in development
+    //    That could have happend in production
     const oldQueue = await db.getQueue(<any>(interaction.guild?.id));
     const connection = connections.get(<any>(interaction.guild?.id));
     if (connection !== undefined) {
